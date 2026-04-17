de2601ee2c534e31a921a2ffb9e3860be2d8d30d
Rajni <ytina3485@gmail.com>
2026-04-17 23:04:30 +0530

    Refactor GoodsBogieTest to use String parameters

--- src/test/java/GoodsBogieTest.java
+++ src/test/java/GoodsBogieTest.java
@@ -5,15 +5,45 @@ class GoodsBogieTest {
 
     @Test
     void testCargo_SafeAssignment() {
-        GoodsBogie bogie = new GoodsBogie(BogieShape.CYLINDRICAL);
-        bogie.assignCargo(CargoType.PETROLEUM);
-        assertEquals(CargoType.PETROLEUM, bogie.getCargo());
+        GoodsBogie bogie = new GoodsBogie("Cylindrical");
+        bogie.assignCargo("Petroleum");
+
+        assertEquals("Petroleum", bogie.getCargo());
     }
 
     @Test
     void testCargo_UnsafeAssignmentHandled() {
-        GoodsBogie bogie = new GoodsBogie(BogieShape.RECTANGULAR);
-        bogie.assignCargo(CargoType.PETROLEUM);
+        GoodsBogie bogie = new GoodsBogie("Rectangular");
+        bogie.assignCargo("Petroleum");
+
+        assertNull(bogie.getCargo()); // should not assign
+    }
+
+    @Test
+    void testCargo_CargoNotAssignedAfterFailure() {
+        GoodsBogie bogie = new GoodsBogie("Rectangular");
+        bogie.assignCargo("Petroleum");
+
         assertNull(bogie.getCargo());
+    }
+
+    @Test
+    void testCargo_ProgramContinuesAfterException() {
+        GoodsBogie bogie = new GoodsBogie("Rectangular");
+
+        bogie.assignCargo("Petroleum"); // fails
+        bogie.assignCargo("Coal");      // should still work
+
+        assertEquals("Coal", bogie.getCargo());
+    }
+
+    @Test
+    void testCargo_FinallyBlockExecution() {
+        GoodsBogie bogie = new GoodsBogie("Cylindrical");
+
+        // Just checking no crash + execution
+        bogie.assignCargo("Coal");
+
+        assertEquals("Coal", bogie.getCargo());
     }
 }
