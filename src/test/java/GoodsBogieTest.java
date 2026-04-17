dc30b6a2ae4d3b5ab817738bd54e330a12a44e5e
Rajni <ytina3485@gmail.com>
2026-04-17 22:52:37 +0530

    Implement unit tests for GoodsBogie class
    Added unit tests for GoodsBogie cargo assignment.

--- src/test/java/GoodsBogieTest.java
+++ src/test/java/GoodsBogieTest.java
@@ -1,1 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class GoodsBogieTest {
+
+    @Test
+    void testCargo_SafeAssignment() {
+        GoodsBogie bogie = new GoodsBogie(BogieShape.CYLINDRICAL);
+        bogie.assignCargo(CargoType.PETROLEUM);
+        assertEquals(CargoType.PETROLEUM, bogie.getCargo());
+    }
+
+    @Test
+    void testCargo_UnsafeAssignmentHandled() {
+        GoodsBogie bogie = new GoodsBogie(BogieShape.RECTANGULAR);
+        bogie.assignCargo(CargoType.PETROLEUM);
+        assertNull(bogie.getCargo());
+    }
+}
